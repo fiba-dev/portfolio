535cc6c9d3f2bdc850e6d892fd6db15a42dc7ef4
fiba-dev <fibarrola06@gmail.com>
2022-07-16 21:51:35 -0400

    change contact form for direct acces

--- src/pages/Contacto/index.tsx
+++ src/pages/Contacto/index.tsx
@@ -4,63 +4,67 @@ import css from "./index.css";
 
 import { sendInfo } from "../../hooks";
 function Contacto() {
-	const navigate = useNavigate();
-	const handlerSubmit = (e) => {
-		e.preventDefault();
+  const navigate = useNavigate();
+  const handlerSubmit = (e) => {
+    e.preventDefault();
 
-		const name = e.target.name.value;
-		const telefono = e.target.email.value;
-		const mensaje =
-			"Asunto:" +
-			e.target.asunto.value +
-			"    Mensaje:" +
-			e.target.mensaje.value;
+    const name = e.target.name.value;
+    const telefono = e.target.email.value;
+    const mensaje =
+      "Asunto:" +
+      e.target.asunto.value +
+      "    Mensaje:" +
+      e.target.mensaje.value;
 
-		sendInfo({ name, telefono, mensaje }).then((res) => {
-			console.log("termino res", res);
-			navigate("/");
-			// setInfoWindows(!infoWindows);
-			// navigate("/");
-		});
-	};
+    sendInfo({ name, telefono, mensaje }).then((res) => {
+      console.log("termino res", res);
+      navigate("/");
+      // setInfoWindows(!infoWindows);
+      // navigate("/");
+    });
+  };
 
-	return (
-		<div className={css.container}>
-			<img
-				className={css.robot}
-				src="https://res.cloudinary.com/fiba06-dev/image/upload/v1646670365/portfolio/robot_2_1_yrfc1h.png"
-			></img>
-			<h1 className={css.titulo}>CONTACTO</h1>
-			<div className={css.div__form}>
-				<form onSubmit={handlerSubmit} className={css.form}>
-					<input
-						name="name"
-						type={"text"}
-						className={css.input}
-						placeholder={"Nombre"}
-					></input>
-					<input
-						name="email"
-						type="email"
-						className={css.input}
-						placeholder={"Email"}
-					></input>
-					<input
-						name="asunto"
-						type={"text"}
-						className={css.input}
-						placeholder={"Asunto"}
-					></input>
-					<textarea
-						name="mensaje"
-						className={css.input2}
-						placeholder={"Mensaje"}
-					></textarea>
-					<button className={css.button}>Enviar</button>
-				</form>
-			</div>
-		</div>
-	);
+  return (
+    <div className={css.container}>
+      <img
+        className={css.robot}
+        src="https://res.cloudinary.com/fiba06-dev/image/upload/v1646670365/portfolio/robot_2_1_yrfc1h.png"
+      ></img>
+      <h1 className={css.titulo}>CONTACTO</h1>
+      <div className={css.div__form}>
+        <a
+          href=" https://wa.me/+5491133180059"
+          target="_blank"
+          className={css.icons}
+        >
+          <img
+            src="https://res.cloudinary.com/fiba06-dev/image/upload/v1658019259/portfolio/pngwing.com_l0ymyv.png"
+            className={css.whatsapp}
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/fran-ibarrola/"
+          className={css.icons}
+          target="_blank"
+        >
+          <img
+            src="https://res.cloudinary.com/fiba06-dev/image/upload/v1658019258/portfolio/pngwing.com_1_lopg2d.png"
+            className={css.linkedin}
+          />
+        </a>
+        <a
+          className={css.icons}
+          target="_blank"
+          href="mailto:fibarrola06@gmail.com"
+        >
+          <img
+            src="https://res.cloudinary.com/fiba06-dev/image/upload/v1658019258/portfolio/pngwing.com_2_hg7ytq.png"
+            className={css.gmail}
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export { Contacto };
