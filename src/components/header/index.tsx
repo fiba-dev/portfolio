200dfd08c8bce3d67d68b0930252a8f64e06a44e
fiba-dev <fibarrola06@gmail.com>
2022-07-24 22:43:15 -0400

    add home icon svg

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,103 +1,102 @@
 import React, { useRef, useState } from "react";
-import { DesktopIcon, MobilIcon } from "../ui/buttons";
+import { DesktopIcon, HomeButton, MobilIcon } from "../ui/buttons";
 import css from "./index.css";
 import { useNavigate } from "react-router-dom";
 import Typed from "typed.js";
 let estado = false;
 
 function Header() {
-	const navigate = useNavigate();
-	const [estado, cambiarEstado] = useState(false);
-	function restartTypedJs(path) {
-		window.location.href = path;
-	}
-	return (
-		<div className={css.main}>
-			<a
-				href="#main"
-				onClick={() => {
-					console.log("SOY RESET");
+  const navigate = useNavigate();
+  const [estado, cambiarEstado] = useState(false);
+  function restartTypedJs(path) {
+    window.location.href = path;
+  }
+  return (
+    <div className={css.main}>
+      <HomeButton
+        href="#main"
+        onClick={() => {
+          console.log("SOY RESET");
 
-					restartTypedJs("#main");
-				}}
-				className={css.home_icon}
-			></a>
-			<div>
-				{estado && (
-					<div className={css.ventana}>
-						<button
-							className={css.close__button}
-							onClick={() => {
-								cambiarEstado(!estado);
-							}}
-						>
-							{" "}
-							X
-						</button>
-						<MobilIcon
-							className={css.mobil__links}
-							onClick={() => {
-								cambiarEstado(!estado);
-							}}
-							href="#main"
-						>
-							Menu{" "}
-						</MobilIcon>
-						<MobilIcon
-							className={css.mobil__links}
-							onClick={() => {
-								cambiarEstado(!estado);
-							}}
-							href="#aboutme"
-						>
-							Perfil
-						</MobilIcon>
+          restartTypedJs("#main");
+        }}
+      ></HomeButton>
+      <div>
+        {estado && (
+          <div className={css.ventana}>
+            <button
+              className={css.close__button}
+              onClick={() => {
+                cambiarEstado(!estado);
+              }}
+            >
+              {" "}
+              X
+            </button>
+            <MobilIcon
+              className={css.mobil__links}
+              onClick={() => {
+                cambiarEstado(!estado);
+              }}
+              href="#main"
+            >
+              Menu{" "}
+            </MobilIcon>
+            <MobilIcon
+              className={css.mobil__links}
+              onClick={() => {
+                cambiarEstado(!estado);
+              }}
+              href="#aboutme"
+            >
+              Perfil
+            </MobilIcon>
 
-						<MobilIcon
-							className={css.mobil__links}
-							onClick={() => {
-								cambiarEstado(!estado);
-								restartTypedJs("#proyectos");
-							}}
-							href="#proyectos"
-						>
-							Proyectos
-						</MobilIcon>
-						<MobilIcon
-							className={css.mobil__links}
-							href="#contacto"
-							onClick={() => {
-								cambiarEstado(!estado);
-								restartTypedJs("#contacto");
-							}}
-						>
-							Contacto{" "}
-						</MobilIcon>
-					</div>
-				)}
-			</div>
-			<div className={css.links_container}>
-				<DesktopIcon className={css.links} href="#aboutme">
-					{" "}
-					Perfil{" "}
-				</DesktopIcon>
+            <MobilIcon
+              className={css.mobil__links}
+              onClick={() => {
+                cambiarEstado(!estado);
+                restartTypedJs("#proyectos");
+              }}
+              href="#proyectos"
+            >
+              Proyectos
+            </MobilIcon>
+            <MobilIcon
+              className={css.mobil__links}
+              href="#contacto"
+              onClick={() => {
+                cambiarEstado(!estado);
+                restartTypedJs("#contacto");
+              }}
+            >
+              Contacto{" "}
+            </MobilIcon>
+          </div>
+        )}
+      </div>
+      <div className={css.links_container}>
+        <DesktopIcon className={css.links} href="#aboutme">
+          {" "}
+          Perfil{" "}
+        </DesktopIcon>
 
-				<DesktopIcon className={css.links} href="#proyectos">
-					{" "}
-					Proyectos{" "}
-				</DesktopIcon>
-				<DesktopIcon className={css.links} href="#contacto">
-					Contacto
-				</DesktopIcon>
-			</div>
-			<button
-				className={css.mobile_menu}
-				onClick={() => {
-					cambiarEstado(!estado);
-				}}
-			></button>
-		</div>
-	);
+        <DesktopIcon className={css.links} href="#proyectos">
+          {" "}
+          Proyectos{" "}
+        </DesktopIcon>
+        <DesktopIcon className={css.links} href="#contacto">
+          Contacto
+        </DesktopIcon>
+      </div>
+      <button
+        className={css.mobile_menu}
+        onClick={() => {
+          cambiarEstado(!estado);
+        }}
+      ></button>
+    </div>
+  );
 }
 
 export { Header };
--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -7,45 +7,45 @@ import { TypedReactHooksDemo } from "../../components/ui/typedJs";
 import { Particulas } from "../../components/ui/particles";
 import { Contacto } from "../../components/Contacto";
 import { Proyectos } from "../../components/Proyectos";
-import { AboutMe } from "../../components/Aboutme";
+import { AboutMe } from "../../components/AboutMe";
 function Home() {
-	return (
-		<div className={css.main} id="main">
-			<div className={css.container}>
-				<div className={css.home__name__div}>
-					<img
-						className={css.robot}
-						src="https://res.cloudinary.com/fiba06-dev/image/upload/v1646670365/portfolio/robot_2_1_yrfc1h.png"
-					></img>
-					<h1 className={css.titulo}>¡Hello World!</h1>
-					<div className={css.typed}>
-						<TypedReactHooksDemo velocidad={30} clase={"letra"} loop={false}>
-							Soy Fran
-						</TypedReactHooksDemo>
-						<TypedReactHooksDemo
-							velocidad={20}
-							comenzar={1000}
-							clase={"letra"}
-							loop={false}
-						>
-							WEB DEVELOPER
-						</TypedReactHooksDemo>
-						<TypedReactHooksDemo
-							velocidad={20}
-							comenzar={2000}
-							clase={"letra2"}
-							loop={true}
-						>
-							React.js Css Html Typescript Firebase Vercel Postman Scrum Github
-						</TypedReactHooksDemo>
-					</div>
-				</div>
-			</div>
-			<AboutMe id="aboutme"></AboutMe>
-			<Proyectos></Proyectos>
-			<Contacto></Contacto>
-		</div>
-	);
+  return (
+    <div className={css.main} id="main">
+      <div className={css.container}>
+        <div className={css.home__name__div}>
+          <img
+            className={css.robot}
+            src="https://res.cloudinary.com/fiba06-dev/image/upload/v1646670365/portfolio/robot_2_1_yrfc1h.png"
+          ></img>
+          <h1 className={css.titulo}>¡Hello World!</h1>
+          <div className={css.typed}>
+            <TypedReactHooksDemo velocidad={30} clase={"letra"} loop={false}>
+              Soy Fran
+            </TypedReactHooksDemo>
+            <TypedReactHooksDemo
+              velocidad={20}
+              comenzar={1000}
+              clase={"letra"}
+              loop={false}
+            >
+              WEB DEVELOPER
+            </TypedReactHooksDemo>
+            <TypedReactHooksDemo
+              velocidad={20}
+              comenzar={2000}
+              clase={"letra2"}
+              loop={true}
+            >
+              React.js Css Html Typescript Firebase Vercel Postman Scrum Github
+            </TypedReactHooksDemo>
+          </div>
+        </div>
+      </div>
+      <AboutMe id="aboutme"></AboutMe>
+      <Proyectos></Proyectos>
+      <Contacto></Contacto>
+    </div>
+  );
 }
 
 export { Home };
