5e3fa864d3af17cfe157c53387e472211c573675
fiba-dev <fibarrola06@gmail.com>
2022-08-05 20:52:57 -0400

    delete href to webpage

--- src/components/ui/cards-proyects/index.tsx
+++ src/components/ui/cards-proyects/index.tsx
@@ -15,39 +15,38 @@ function CardProyects(props) {
 		<div className={claseDiv}>
 			<div className={css.container}>
 				<div className={css.card}>
-					<a href={props.webPage} target="_blank">
-						<div className={[css.card_front, css.face].join(" ")}>
-							<a>
-								<img
-									onClick={props.handlerImage}
-									src={props.pictureURL}
-									className={css.imagen}
-								></img>
-							</a>{" "}
-						</div>
-						<div className={[css.card_back, css.face].join(" ")}>
-							<h1>{props.title}</h1>
-							<p>{props.info}</p>
+					<div className={[css.card_front, css.face].join(" ")}>
+						<a>
+							<img
+								onClick={props.handlerImage}
+								src={props.pictureURL}
+								className={css.imagen}
+							></img>
+						</a>{" "}
+					</div>
 
-							<div className={css.links__container}>
-								<a
-									href={props.webPage}
-									target="_blank"
-									className={css.informacion}
-								>
-									Web-Page
-								</a>
+					<div className={[css.card_back, css.face].join(" ")}>
+						<h1>{props.title}</h1>
+						<p>{props.info}</p>
 
-								<a
-									className={css.informacion}
-									target="_blank"
-									href={props.GitHub}
-								>
-									Git-Hub
-								</a>
-							</div>
+						<div className={css.links__container}>
+							<a
+								href={props.webPage}
+								target="_blank"
+								className={css.informacion}
+							>
+								Web-Page
+							</a>
+
+							<a
+								className={css.informacion}
+								target="_blank"
+								href={props.GitHub}
+							>
+								Git-Hub
+							</a>
 						</div>
-					</a>
+					</div>
 				</div>
 			</div>
 		</div>
