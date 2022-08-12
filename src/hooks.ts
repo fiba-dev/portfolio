9b4495737ce51c2ad63a55bc34fb21d9500875d7
fiba-dev <fibarrola06@gmail.com>
2022-08-12 16:58:40 -0400

    delete import apiURL in hooks

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,22 +1,3 @@
 import react from "react";
 
-import { API_URL } from "./apiURL";
-
-export async function sendInfo(params) {
-	const res = await fetch(API_URL + "/contact", {
-		method: "post",
-		headers: {
-			"content-type": "application/json",
-		},
-		body: JSON.stringify({
-			donde: params.mensaje,
-			name: params.name,
-
-			telefono: params.telefono,
-		}),
-	});
-
-	const data = await res.json();
-
-	return data;
-}
+export async function sendInfo(params) {}
