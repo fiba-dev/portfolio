010bb0d7548ed3ccfc4567509447ec3932d54ea7
fiba-dev <fibarrola06@gmail.com>
2022-08-29 19:54:57 -0400

    delete consolelog

--- src/components/ui/cards-proyects/index.tsx
+++ src/components/ui/cards-proyects/index.tsx
@@ -4,55 +4,45 @@ import { Link } from "react-router-dom";
 import Zoom from "react-reveal/Zoom";
 
 function CardProyects(props) {
-	let claseDiv;
-	console.log("SOY CARDS", props);
-
-	if (props.id % 2 == 0) {
-		claseDiv = css.div1;
-	} else {
-		claseDiv = css.div2;
-	}
 	return (
-		<div className={claseDiv}>
-			<Zoom key={props.id}>
-				<div className={css.container}>
-					<div className={css.card}>
-						<div className={[css.card_front, css.face].join(" ")}>
-							<a>
-								<img
-									onClick={props.handlerImage}
-									src={props.pictureURL}
-									className={css.imagen}
-								></img>
-							</a>{" "}
-						</div>
+		<Zoom key={props.id}>
+			<div className={css.container}>
+				<div className={css.card}>
+					<div className={[css.card_front, css.face].join(" ")}>
+						<a>
+							<img
+								onClick={props.handlerImage}
+								src={props.pictureURL}
+								className={css.imagen}
+							></img>
+						</a>{" "}
+					</div>
 
-						<div className={[css.card_back, css.face].join(" ")}>
-							<h1>{props.title}</h1>
-							<p>{props.info}</p>
+					<div className={[css.card_back, css.face].join(" ")}>
+						<h1>{props.title}</h1>
+						<p>{props.info}</p>
 
-							<div className={css.links__container}>
-								<a
-									href={props.webPage}
-									target="_blank"
-									className={css.informacion}
-								>
-									Web-Page
-								</a>
+						<div className={css.links__container}>
+							<a
+								href={props.webPage}
+								target="_blank"
+								className={css.informacion}
+							>
+								Web-Page
+							</a>
 
-								<a
-									className={css.informacion}
-									target="_blank"
-									href={props.GitHub}
-								>
-									Git-Hub
-								</a>
-							</div>
+							<a
+								className={css.informacion}
+								target="_blank"
+								href={props.GitHub}
+							>
+								Git-Hub
+							</a>
 						</div>
 					</div>
 				</div>
-			</Zoom>
-		</div>
+			</div>
+		</Zoom>
 	);
 }
 export { CardProyects };
