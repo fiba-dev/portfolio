a24cf749e3f25e95589921062d175e11bf0810b2
fiba-dev <fibarrola06@gmail.com>
2023-12-04 18:57:45 -0400

    change index

--- webpack.config.js
+++ webpack.config.js
@@ -41,7 +41,7 @@ module.exports = (env, argv) => {
 		},
 		plugins: [
 			new HtmlWebpackPlugin({
-				template: "public/index.html",
+				template: "index.html",
 				inject: "body",
 			}),
 		],
