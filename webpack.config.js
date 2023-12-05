67fe195de514c538d1bd349b5c7726a7a5c07889
fiba-dev <fibarrola06@gmail.com>
2023-12-05 13:51:45 -0400

    change webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,10 @@
 const path = require("path");
-const dev = process.env.NODE_ENV == "development";
-const liveServer = require("live-server");
-if (dev) {
-	liveServer.start({
-		root: "./",
-		file: "index.html",
-	});
-}
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+const dev = process.env.NODE_ENV === "development";
 
 module.exports = {
-	watch: dev,
+	mode: dev ? "development" : "production",
 	entry: "./src/index.tsx",
 	module: {
 		rules: [
@@ -22,12 +17,7 @@ module.exports = {
 				test: /\.css$/i,
 				use: [
 					"style-loader",
-					{
-						loader: "css-loader",
-						options: {
-							modules: true,
-						},
-					},
+					{ loader: "css-loader", options: { modules: true } },
 				],
 			},
 		],
@@ -39,4 +29,18 @@ module.exports = {
 		path: path.resolve(__dirname, "dist"),
 		filename: "bundle.js",
 	},
+	devServer: {
+		static: {
+			directory: path.join(__dirname, "public"),
+		},
+		compress: true,
+		port: 8080,
+		historyApiFallback: true,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: __dirname + "/index.html",
+			filename: "index.html",
+		}),
+	],
 };
